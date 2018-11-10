61f55965e8d809e95acf9e46b67d7d903acb7db6
Lior Lavon <>
2018-11-10 19:13:00 +0100

    adding mongoose MongoDB schema

---
+++ server/api/post/postSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String, 
+        unique: true,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    categories: [
+            {type: Schema.Types.ObjectId, ref: 'category'}
+        ],
+});
+
+module.exports = mongoose.model('post', PostSchema);
